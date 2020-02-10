5e5da42c1a58bcdb588734217fdc195e50624657
Alexander Anpleenko <avanpleenko@gmail.com>
2020-02-10 23:45:12 +0300

    fix lint-staged config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'app/**/*.{ts,tsx}': ['eslint --fix', 'prettier-eslint --write', 'git add'],
+  '*.{ts,tsx}': ['eslint --fix', 'prettier-eslint --write'],
   '*.{png,jpeg,jpg,gif,svg}': ['imagemin-lint-staged', 'git add'],
 };
