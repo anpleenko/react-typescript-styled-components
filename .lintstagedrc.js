af4b748ae7ecd2243d32675260cc90e239e9b52d
Alexander Anpleenko <avanpleenko@gmail.com>
2020-02-11 02:28:42 +0300

    fix stage comand

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-  '**/*.ts?(x)': () => ['prettier-eslint --write', 'git add'],
-  '**/*.{png,jpeg,jpg,gif,svg}': () => ['imagemin-lint-staged', 'git add'],
+  '*.{ts,tsx}': ['eslint --fix', 'prettier-eslint --write', 'git add'],
+  '*.{png,jpeg,jpg,gif,svg}': ['imagemin-lint-staged', 'git add'],
 };
