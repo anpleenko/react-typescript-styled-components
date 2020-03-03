c3906e56935ad311fef4fa8a4cee4d29bac6bf32
Alexander Anpleenko <avanpleenko@gmail.com>
2020-03-03 22:46:55 +0300

    return create-react-app files

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
-import * as React from 'react';
-import { render } from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 import { App } from './App';
+import * as serviceWorker from './serviceWorker';
 
-const rootElement = document.getElementById('root');
-render(<App />, rootElement);
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster,  y ou can change
+// unregister() to register() below. Note this comes with some  pi tf alls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
